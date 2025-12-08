84c85ffaa69e7999c389095b8ab3fe5c7e7767e0
mehdi1363sh <mehdi.sh2006@gmail.com>
2025-12-08 15:55:17 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
+// سرو کردن فایل‌های استاتیک
+app.use(express.static(path.join(__dirname, "public")));
+
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "*" }
+  cors: { origin: "*" }
 });
 
-app.use(express.static('public'));
-
+// لیست بازیکنان
 let players = [];
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  socket.on("join", (name) => {
+    players.push({ id: socket.id, name });
+    io.emit("playersUpdate", players);
+  });
 
-    socket.on('join', (name) => {
-        socket.name = name;
-        players.push(name);
-        io.emit('players', players);
-    });
+  socket.on("disconnect", () => {
+    players = players.filter(p => p.id !== socket.id);
+    io.emit("playersUpdate", players);
+  });
+});
 
-    socket.on('disconnect', () => {
-        players = players.filter(p => p !== socket.name);
-        io.emit('players', players);
-    });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
+server.listen(PORT, () => console.log("Server running on port " + PORT));
